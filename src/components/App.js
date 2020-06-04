554a70545cd038655efc449ac2fb8ac2f49fbacd
leopoldkwok <leopoldkwok@gmail.com>
2020-06-04 21:23:29 +0100

    Crafting a Form Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import {Form, FormControl, Button} from "react-bootstrap";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return <div>ageteller component</div>;
+    return (
+      <div className="App">
+        <Form inline>
+          <h2>Input Your Birthday!</h2>
+          <FormControl type="date"></FormControl>
+          <Button>Submit</Button>
+        </Form>
+      </div>
+    );
   }
 }
 
